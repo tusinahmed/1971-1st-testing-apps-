32e45e23f97b7b10778552b93870c85abb5d0fed
tusinahmed <tusinahmed@gmail.com>
2016-06-11 19:41:48 +0600

    Revert "Revert "1st""
    This reverts commit 52812d5c10134072c97d8134c7c9981259f37312.

---
+++ app/src/main/java/com/example/tusin/button/info.java
@@ -0,0 +1,17 @@
+package com.example.tusin.button;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by tusin on 5/31/2016.
+ */
+public class info Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+
+    }
+}
